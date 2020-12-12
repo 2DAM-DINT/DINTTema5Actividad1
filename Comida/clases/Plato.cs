99f1ba53b83b464dd57fa2656c17590dc4c1ca41
Adrián <adrian.cuenca@iesdoctorbalmis.com>
2020-12-12 15:35:21 +0100

    Agregar archivos de proyecto.

---
+++ Comida/clases/Plato.cs
@@ -0,0 +1,143 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Comida
+{
+    class Plato : INotifyPropertyChanged
+    {
+        private string nombre;
+        private string imagen;
+        private string tipo;
+        private bool gluten;
+        private bool soja;
+        private bool leche;
+        private bool sulfitos;
+
+        public string Nombre 
+        {
+            get { return this.nombre; }
+            set
+            {
+                if (this.nombre != value)
+                {
+                    this.nombre = value;
+                    this.NotifyPropertyChanged("Nombre");
+                }
+            }
+        }
+
+        public string Imagen 
+        {
+            get { return this.imagen; }
+            set
+            {
+                if (this.imagen != value)
+                {
+                    this.imagen = value;
+                    this.NotifyPropertyChanged("Imagen");
+                }
+            }
+        }
+
+        public string Tipo 
+        {
+            get { return this.tipo; }
+            set
+            {
+                if (this.tipo != value)
+                {
+                    this.tipo = value;
+                    this.NotifyPropertyChanged("Tipo");
+                }
+            }
+        }
+
+        public bool Gluten 
+        {
+            get { return this.gluten; }
+            set
+            {
+                if (this.gluten != value)
+                {
+                    this.gluten = value;
+                    this.NotifyPropertyChanged("Gluten");
+                }
+            }
+        }
+
+        public bool Soja 
+        {
+            get { return this.soja; }
+            set
+            {
+                if (this.soja != value)
+                {
+                    this.soja = value;
+                    this.NotifyPropertyChanged("Soja");
+                }
+            }
+        }
+
+        public bool Leche 
+        {
+            get { return this.leche; }
+            set
+            {
+                if (this.leche != value)
+                {
+                    this.leche = value;
+                    this.NotifyPropertyChanged("Leche");
+                }
+            }
+        }
+
+        public bool Sulfitos 
+        {
+            get { return this.sulfitos; }
+            set
+            {
+                if (this.sulfitos != value)
+                {
+                    this.sulfitos = value;
+                    this.NotifyPropertyChanged("Sulgitos");
+                }
+            }
+        }
+
+        public Plato(string nombre, string imagen, string tipo, bool gluten, bool soja, bool leche, bool sulfitos)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Tipo = tipo;
+            Gluten = gluten;
+            Soja = soja;
+            Leche = leche;
+            Sulfitos = sulfitos;
+        }
+
+        public Plato()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static List<Plato> GetSamples(string rutaImagenes)
+        {
+            List<Plato> lista = new List<Plato>();
+
+            lista.Add(new Plato("Hamburguesa", Path.Combine(rutaImagenes, @"./recursos/platos/burguer.jpg"), "Americana", true, false, true, true));
+            lista.Add(new Plato("Dumplings", Path.Combine(rutaImagenes, @"./recursos/platos/dumplings.jpg"), "China", true, true, false, false));
+            lista.Add(new Plato("Tacos", Path.Combine(rutaImagenes, @"./recursos/platos/tacos.jpg"), "Mexicana", true, false, false, true));
+            lista.Add(new Plato("Cerdo agridulce", Path.Combine(rutaImagenes, @"./recursos/platos/cerdoagridulce.jpg"), "China", true, true, false, true));
+            lista.Add(new Plato("Hot dogs", Path.Combine(rutaImagenes, @"./recursos/platos/hotdog.jpg"), "Americana", true, true, true, true));
+            lista.Add(new Plato("Fajitas", Path.Combine(rutaImagenes, @"./recursos/platos/fajitas.jpg"), "Mexicana", true, false, false, true));
+
+            return lista;
+        }
+    }
+}
