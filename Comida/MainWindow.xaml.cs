edbcd6fa78603ace658c451e83be1159e77a8070
Adrián <adrian.cuenca@iesdoctorbalmis.com>
2020-12-12 20:05:15 +0100

    Finalizo actividad

--- Comida/MainWindow.xaml.cs
+++ Comida/MainWindow.xaml.cs
@@ -7,11 +7,19 @@ namespace Comida
     {
         private List<Plato> Platos { get; set; }
 
+        private List<string> tiposValidos;
+
         public MainWindow()
         {
             InitializeComponent();
             Platos = Plato.GetSamples(@".\recursos\platos");
             listaPlatos.DataContext = Platos;
+
+            tiposValidos = new List<string>();
+            tiposValidos.Add("Americana");
+            tiposValidos.Add("Mexicana");
+            tiposValidos.Add("China");
+            listaTipos.ItemsSource = tiposValidos;
         }
     }
 }
